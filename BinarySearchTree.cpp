77ebcf48960e7435ec671bf13f63c6dc593e3bee
ArbyPangestu <arbypangestu340@gmail.com>
2025-06-26 11:09:35 +0700

    Refactor BinaryTree class for improved readability and consistency in comments

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -1,145 +1,130 @@
 #include <iostream>
 using namespace std;
 
-class Node
+class Node 
 {
-public: 
-    int info;
-    Node *leftchild;
-    Node *rightchild;
+    public:
+        int info;
+        Node *leftchild;
+        Node *rightchild;
 
-    // Contruktor untuk class node
-    Node()
-    {
-        leftchild = nullptr; // Initialize left child to null
-        rightchild = nullptr; // Initialize right child to null
-    }
+        //constructor untuk class node
+        Node()
+        {
+            leftchild = nullptr; //inisialisasi left child jadi null
+            rightchild = nullptr; //inisialisasi right child jadi null
+        }
 };
 
 class BinaryTree
 {
-public: 
-    Node *ROOT;
+    public:
+        Node *ROOT;
 
-    BinaryTree()
-    {
-        ROOT = nullptr; // Initializing ROOT to null
-    }
-
-    void insert()
-    {
-        int x;
-        cout << "Masukan nilai: ";
-        cin >> x;
-
-        // Step 1: Mengalokasikan memori untuk new node
-        Node *newNode = new Node();
-
-        // Step 2 : Assign value to the data field of new node
-        newNode->info = x;
-
-        // Step 3 : Make the left and right child of the new node point to NULL
-        newNode->leftchild = nullptr;
-        newNode->rightchild = nullptr;
-
-        // Step 4 : Locate the node whitch will be the parent of the node to be inserted
-        Node *parent = nullptr;
-        Node *currentNode = nullptr;
-        search(x, parent, currentNode);
-
-        // Step 5 : If parent is NULL (Tree is empty)
-        if (parent == nullptr)
+        BinaryTree()
         {
-            // 5a : Mark the new node as ROOT
-            ROOT = newNode;
-
-            // 5b : Exit
-            return;
+            ROOT = nullptr; //inisialisasi root jadi null
         }
 
-        // Step 6 : If the value in the data filed of new node is less than that of parent
-        if (x < parent->info)
+        void insert()
         {
-            // 6a : Make the left child of the paren to the new node
-            parent->leftchild = newNode;
+            int x;
+            cout << "Masukkan Sebuah Nilai = ";
+            cin >> x;
 
-            // 6b : Exit
-            return;
-        }
+            //step selanjutnya mengalokasikan memori untuk node baru
+            Node *newNode = new Node();
 
-        // Step 7 : If the value in the data field of the new node is greater than that of the parent
-        else if (x > parent->info)
-        {
-            // 7a : Make the right child of parent point to the new node
-            parent->rightchild = newNode;
+            //setelah itu, kita akan mengisi informasi pada node baru tersebut
+            newNode->info = x;
 
-            // 7b : Exit
-            return;
-        }
-    }
+            newNode->leftchild = nullptr; //inisialisasi left child jadi null
+            newNode->rightchild = nullptr; //inisialisasi right child jadi null
 
-    void search(int elemnt, Node *&parent, Node *&currentNode)
-    {
-        // The function search the currentNode of the specifide Node as well as the current Node of its parent
-        currentNode = ROOT;
-        parent = nullptr;
-        while ((currentNode != nullptr) && (currentNode->info != elemnt))
-        {
-            parent = currentNode;
-            if (elemnt < currentNode->info)
-                currentNode = currentNode->leftchild;
-            else
-                currentNode = currentNode->rightchild;
+            Node *parent = nullptr;
+            Node *currentNode = nullptr;
+            search(x, parent, currentNode);
+
+            if (parent == nullptr)
+            {
+                ROOT = newNode;
+
+                return;
             }
-    }
 
-    void inorder(Node *ptr)
-    {
-        if (isEmpty())
-        {
-            cout << "Tree is empty" << endl;
-            return;
+            if (x < parent->info)
+            {
+                parent->leftchild = newNode;
+                return;
+            }
+            else if (x > parent->info)
+            {
+                parent->rightchild = newNode;
+                return;
+            }
+
         }
-        if (ptr == nullptr)
-            return;
 
-        inorder(ptr->leftchild);
-        cout << ptr->info << " "; // Parent
-        inorder(ptr->rightchild);
-    }
-
-    void preorder(Node *ptr)
-    {
-        if (isEmpty())
+        void search(int element, Node *&parent, Node *&currentNode)
         {
-            cout << "Tree is empty" << endl;
-            return;
+            currentNode = ROOT;
+            parent = nullptr;
+            while ((currentNode != nullptr) && (currentNode->info != element))
+            {
+                parent = currentNode;
+                if (element < currentNode->info)
+                    currentNode = currentNode->leftchild;
+                else
+                    currentNode = currentNode->rightchild;
+            }
         }
-        if (ptr == nullptr)
-            return;
 
-        cout << ptr->info << " "; // Parent
-        preorder(ptr->leftchild);
-        preorder(ptr->rightchild);
-    }
-
-    void postorder(Node *ptr)
-    {
-        if (isEmpty())
+        void inorder(Node *ptr)
         {
-            cout << "Tree is empty" << endl;
-            return;
+            if (isEmpty())
+            {
+                cout << "Tree dalam kondisi Kosong" << endl;
+                return;
+            }
+            if (ptr == nullptr)
+                return;
+
+            inorder(ptr->leftchild);
+            cout << ptr->info << " ";
+            inorder(ptr->rightchild);
         }
-        if (ptr == nullptr)
-            return;
+        
+        void preorder(Node *ptr)
+        {
+            if (isEmpty())
+            {
+                cout << "Tree dalam kondisi Kosong" << endl;
+                return;
+            }
+            if (ptr == nullptr)
+                return;
 
-        postorder(ptr->leftchild);
-        postorder(ptr->rightchild);
-        cout << ptr->info << " "; // Parent
-    }
+            cout << ptr->info << " ";
+            preorder(ptr->leftchild);
+            preorder(ptr->rightchild);
+        }
 
+        void postorder(Node *ptr)
+        {
+            if (isEmpty())
+            {
+                cout << "Tree dalam kondisi Kosong" << endl;
+                return;
+            }
+            if (ptr == nullptr)
+                return;
 
-    bool isEmpty()
+            postorder(ptr->leftchild);
+            postorder(ptr->rightchild);
+            cout << ptr->info << " ";
+        }
+
+        bool isEmpty()
         {
             return ROOT == nullptr;
         }
@@ -158,8 +143,41 @@ int main()
         cout << "5. Keluar" << endl;
         cout << "\nMasukan Pilihan Anda (1-5) : ";
 
-        
+        char ch;
+        cin >> ch;
+        cout << endl;
+
+        switch (ch)
+        {
+        case '1':
+        {
+            x.insert();
+            break;
+        }
+        case '2':
+        {
+            x.inorder(x.ROOT);
+            break;
+        }
+        case '3':
+        {
+
+            x.preorder(x.ROOT);
+            break;
+        }
+        case '4':
+        {
+
+            x.postorder(x.ROOT);
+            break;
+        }
+        case '5':
+            return 0;
+        default:
+        {
+            cout << "Pilihan tidak valid" << endl;
+            break;
+        }
+        }
     }
 };
-
-
