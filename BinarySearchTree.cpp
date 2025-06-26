e5c8e1c7389b2d8cd8ce61d87a5cd434315d2caf
ArbyPangestu <arbypangestu340@gmail.com>
2025-06-26 10:43:45 +0700

    Insert new node as left child if value is less than parent in insert method

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -57,6 +57,17 @@ public:
             return;
         }
 
+        // Step 6 : If the value in the data filed of new node is less than that of parent
+        if (x < parent->info)
+        {
+            // 6a : Make the left child of the paren to the new node
+            parent->leftchild = newNode;
+
+            // 6b : Exit
+            return;
+        }
+
         
     }
+
 };
